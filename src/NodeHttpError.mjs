398ef2214ad870fc8f54e4e1cad04d94810ae5ab
Ankit Gandhi <ankitgandhi452@gmail.com>
2023-01-05 14:24:27 +0530

    Removed: CommonJS Support & Tests; Updated: Service Name & Handled Logging

--- src/NodeHttpError.mjs
+++ src/NodeHttpError.mjs
@@ -1,3 +1,5 @@
+import { SERVICE } from './CONFIG.mjs'
+
 const DEFAULT_ERROR_MSG = 'Node Http Error'
 const DEFAULT_ERROR_STATUS_CODE = 500
 const DEFAULT_ERROR_CODE = 'NODE_HTTP_ERROR'
@@ -17,15 +19,9 @@ export default class NodeHttpError extends Error {
       code: eCode
     } = e
 
-    const {
-      npm_package_name: pkgName = '',
-      npm_package_version: pkgVersion = ''
-    } = process.env
-    const service = `${pkgName}@${pkgVersion}`
-
     this._isCustomError = true
     this._isNodeHttpError = true
-    this.service = service
+    this.service = SERVICE
     this.message = message || eMessage || eMsg || DEFAULT_ERROR_MSG
     this.statusCode = statusCode || eStatusCode || DEFAULT_ERROR_STATUS_CODE
     this.errorCode = errorCode || eErrorCode || eCode || DEFAULT_ERROR_CODE
